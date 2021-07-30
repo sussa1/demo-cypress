e6427a6a538eeef8e29cba7dbb035b572b329df4
Vitor <assuncao26vitor@gmail.com>
2021-07-30 17:52:36 -0300

    Tarefa 3

--- cypress/integration/meu_teste_end_to_end.js
+++ cypress/integration/meu_teste_end_to_end.js
@@ -16,5 +16,13 @@ describe('Teste End-to-End', () => {
 
         // Fecha o pop-up com o preço do frete
         cy.get('.swal-button').click()
+
+        // Faz uma compra
+        cy.contains('Comprar').click().then
+        cy.wait(2000)
+        cy.get('.swal-text').contains('Sua compra foi realizada com sucesso')
+
+        // Fecha o pop-up com a confirmação da compra
+        cy.get('.swal-button').click()
     })
   })
